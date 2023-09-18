a0c266ced6af8dbfa6592a37314e3ff3963f48e7
harshkumarchourasia <harshkumarchourasia@gmail.com>
2023-09-18 21:10:27 +0530

    :fire: solution for https://cses.fi/problemset/task/1069 added

---
+++ src/bin/Repetitions.rs
@@ -0,0 +1,24 @@
+// https://cses.fi/problemset/task/1069
+
+use std::io::stdin;
+fn main(){
+    let mut inp = String::new();
+    stdin().read_line(&mut inp).expect("Error in input");
+    let mut res = 0;
+    let mut temp = 1;
+    let mut prev_char = inp.chars().nth(0).expect("error");
+    for (idx,char) in inp.chars().enumerate(){
+        if idx==0{
+            continue;
+        }
+        if char==prev_char{
+            temp+=1;
+        }
+        else{
+            temp = 1
+        }
+        res=res.max(temp);
+        prev_char=char;
+    }
+    println!("{}", res);
+}
